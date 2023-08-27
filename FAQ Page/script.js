db4c3e685e14fb3deedfb34d55989129c385dc11
Manaslohe <manas.lohe2021@vitstudent.ac.in>
2023-08-27 15:03:29 +0530

    minor update

--- FAQ Page/script.js
+++ FAQ Page/script.js
@@ -1,12 +1,16 @@
-// - Bring in toggle buttons (querySelectorAll)
-// - Loop through nodeList (forEach)
-// - Add click event (addEventListener)
-// - Toggle the active class on the parent node (.parentNode & classList.toggle())
-
-const toggles = document.querySelectorAll('.faq-toggle')
+const toggles = document.querySelectorAll('.faq-toggle');
 
 toggles.forEach(toggle => {
-    toggle.addEventListener('click',()=>{
-        toggle.parentNode.classList.toggle('active')
-    })
-})
+  toggle.addEventListener('click', () => {
+    const parent = toggle.parentNode;
+    const content = parent.querySelector('.faq-container');
+    
+    parent.classList.toggle('active');
+    
+    if (parent.classList.contains('active')) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = '0';
+    }
+  });
+});
